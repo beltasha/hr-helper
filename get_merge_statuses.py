793e99301198d574767e1d1e18085210edc04f52
Natalia Beliaeva <nataly9682@gmail.com>
2019-04-04 09:42:55 +0300

    add parametrized query

--- get_merge_statuses.py
+++ get_merge_statuses.py
@@ -6,37 +6,36 @@ import config
 authorization_token = "Bearer {0}".format(config.TOKEN)
 headers = {"Authorization": authorization_token}
 
-def run_query(query): # A simple function to use requests.post to make the API call. Note the json= section.
+def run_query(query):
     request = requests.post('https://api.github.com/graphql', json={'query': query}, headers=headers)
     if request.status_code == 200:
         return request.json()
     else:
         raise Exception("Query failed to run by returning code of {}. {}".format(request.status_code, query))
 
-# The GraphQL query (with a few aditional bits included) itself defined as a multi-line string.       
-query = """
-{
-  repositoryOwner(login: "IvanGoncharov") {
-    repositories(last: 10) {
-      nodes {
-        name
-        description,
-        pullRequests (last: 2) {
-          nodes {
-            commits (last: 10){
-              nodes {
-                commit {
-                  commitUrl,
-                  status {
-                    state,
+def get_query(login, repositories_count, pull_rquest_count, commit_count):
+  return """ {
+    repositoryOwner(login: {0}) {
+      repositories(last: {1}) {
+        nodes {
+          name
+          description,
+          pullRequests (last: {2}) {
+            nodes {
+              commits (last: {3}){
+                nodes {
+                  commit {
+                    commitUrl,
+                    status {
+                      state,
+                    }
                   }
                 }
-              }
-            } 
+              } 
+            }
           }
         }
       }
     }
-  }
-}
-"""
+  } """.format(login, repositories_count, pull_rquest_count, commit_count)
+
